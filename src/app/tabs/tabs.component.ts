cabc161a990f2223f8229b638b2fc110d95b00b0
Sebastien George <sebgrge04@gmail.com>
2024-08-08 16:28:01 +0200

    handle presentation

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrl: './tabs.component.css'
 })
 export class TabsComponent {
+  characters = [
+    { name: 'Luke Skywalker', side: '' },
+    { name: 'Darth Vader', side: '' }
+  ];
 
+  chosenList = 'all';
+
+  getCharacters(){
+    if(this.chosenList === 'all') {
+      return this.chosenList.slice();
+    }
+    return this.characters.filter((char) => {
+      return char.side === this.chosenList;
+    })
+  }
+
+  onChoose(side){
+    this.chosenList = side;
+  }
 }
